8bda811c6eda5e9a73a45447f9bf3ea2b8a35436
jiyinyiyong <jiyinyiyong@gmail.com>
2017-09-20 01:48:42 +0800

    first working example

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Minimal tiny app
+----
+
+> minimal code to run create a Respo app.
+
+Based on [Respo/tiny-app](https://github.com/Respo/tiny-app). Browse `src/app/main.cljs` for details.
+
+### License
+
+MIT
---
+++ src/app/main.cljs
@@ -0,0 +1,48 @@
+
+(ns app.main
+  (:require-macros [tiny-app.core :refer [create-tiny-app->]]
+                   [respo.macros :refer [defcomp <> div span button]])
+  (:require [respo.core]
+            [respo.cursor]))
+
+; need to require respo.core and respo.cursor since code in Macros need them
+
+(def store {:count 0})
+
+(defn updater [store op op-data]
+  (case op
+    :inc (update store :count inc)
+    nil))
+
+(def style-button
+  {:border :none
+   :outline :none
+   :background-color "#aac"
+   :display :inline-block
+   :color :white
+   :border-radius "0px"
+   :cursor :pointer
+   :font-size 20})
+
+(defn on-inc [e dispatch! mutate!]
+  (dispatch! :inc nil))
+
+(defcomp comp-container [store]
+  (div {}
+    (button {:on {:click on-inc}
+             :inner-text "inc"
+             :style style-button})
+    (<> (:count store))))
+
+(def app
+  (create-tiny-app->
+    {:model store
+     :updater updater
+     :view comp-container
+     :mount-target (.querySelector js/document ".app")
+     :ssr? false
+     :show-ops? true}))
+
+(def reload! (:reload! app))
+
+(set! (.-onload js/window) (:start-app! app))

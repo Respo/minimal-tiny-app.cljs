519e8a3b44d96ecc0b8a07d5ab4b106e1005dddb
jiyinyiyong <jiyinyiyong@gmail.com>
2017-10-26 14:20:01 +0800

    still simplifying example

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -3,33 +3,26 @@
   (:require [respo.macros :refer [defcomp <> div span button]]
             [tiny-app.core :refer [create-tiny-app->]]))
 
-; need to require respo.core and respo.cursor since code in Macros need them
-
 (def store {:count 0, :states 0})
 
 (defn updater [store op op-data]
   (case op
     :inc (update store :count inc)
-    nil))
-
-(def style-button
-  {:border :none
-   :outline :none
-   :background-color "#aac"
-   :display :inline-block
-   :color :white
-   :border-radius "0px"
-   :cursor :pointer
-   :font-size 20})
-
-(defn on-inc [e dispatch! mutate!]
-  (dispatch! :inc nil))
+    store))
 
 (defcomp comp-container [store]
   (div {}
-    (button {:on {:click on-inc}
-             :inner-text "inc"
-             :style style-button})
+    (button {:inner-text "inc"
+             :style {:border :none
+                      :outline :none
+                      :background-color "#aac"
+                      :display :inline-block
+                      :color :white
+                      :border-radius "0px"
+                      :cursor :pointer
+                      :font-size 20}
+             :on {:click (fn [e dispatch! mutate!]
+                            (dispatch! :inc nil))}})
     (<> (:count store))))
 
 (def app
